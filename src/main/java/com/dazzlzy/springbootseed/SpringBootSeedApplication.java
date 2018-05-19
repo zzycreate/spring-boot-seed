40949573288364c612f10e33e3db7f0ca5dbda38
dazzlzy <dazzlzy@gmail.com>
2018-05-19 09:19:38 +0800

    init

---
+++ src/main/java/com/dazzlzy/springbootseed/SpringBootSeedApplication.java
@@ -0,0 +1,12 @@
+package com.dazzlzy.springbootseed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSeedApplication.class, args);
+    }
+}
