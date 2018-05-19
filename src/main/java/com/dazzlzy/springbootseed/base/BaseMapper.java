1bee267535ef07b28b1b8b6332bb7f52db7cd28d
dazzlzy <dazzlzy@gmail.com>
2018-05-20 01:53:19 +0800

    BaseMapper增加@Mapper接口，自动注入子类接口

--- src/main/java/com/dazzlzy/springbootseed/base/BaseMapper.java
+++ src/main/java/com/dazzlzy/springbootseed/base/BaseMapper.java
@@ -1,5 +1,6 @@
 package com.dazzlzy.springbootseed.base;
 
+import org.apache.ibatis.annotations.Mapper;
 import tk.mybatis.mapper.common.ConditionMapper;
 import tk.mybatis.mapper.common.ExampleMapper;
 import tk.mybatis.mapper.common.IdsMapper;
@@ -11,6 +12,7 @@ import tk.mybatis.mapper.common.special.InsertListMapper;
  * @author dazzlzy
  * @date 2018/4/1
  */
+@Mapper
 public interface BaseMapper<T> extends tk.mybatis.mapper.common.BaseMapper<T>,
         ExampleMapper<T>, ConditionMapper<T>, IdsMapper<T>, InsertListMapper<T> {
 }
