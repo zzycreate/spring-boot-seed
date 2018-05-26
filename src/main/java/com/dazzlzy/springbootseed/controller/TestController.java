134299b20767aab77e398306ae059f6b73659e5d
dazzlzy <dazzlzy@gmail.com>
2018-05-26 13:37:14 +0800

    swagger2 优化

--- src/main/java/com/dazzlzy/springbootseed/controller/TestController.java
+++ src/main/java/com/dazzlzy/springbootseed/controller/TestController.java
@@ -5,13 +5,11 @@ import com.dazzlzy.common.base.BaseResultGenerator;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 测试Controller
+ * 使用@RestController可以不需要使用@ResponseBody注解，方法会自动处理成json格式返回
  *
  * @author dazzlzy
  * @date 2018/5/22
@@ -24,12 +22,12 @@ public class TestController {
 
     /**
      * 测试
+     * 使用@GetMapping相当于使用@RequestMapping(method={RequestMethod.GET})
      *
      * @return BaseResult
      */
     @ApiOperation(value = "测试", notes = "测试接口")
-    @RequestMapping(value = "test", method = {RequestMethod.GET})
-    @ResponseBody
+    @GetMapping(value = "test")
     public BaseResult test() {
         log.info("test info ...");
         return BaseResultGenerator.success();
